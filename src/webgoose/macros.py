1521ee5c74768e6c2f4b0b2975c20544d34f0e92
Travis Campbell <traviscampbell@hotmail.co.uk>
2022-11-16 00:25:41 +0000

    Added Page Class (seperates getters from page_builder) - Changed Some Macro Stuff - Requires HELLA Work Neatening Up

--- src/webgoose/macros.py
+++ src/webgoose/macros.py
@@ -1,48 +1,53 @@
 
-from datetime import datetime
-import time
 import os
+import time
+from datetime import datetime
 
+from src.webgoose import version as webgoose_version
 from src.webgoose.config import config
 from src.webgoose.file_traverser import FileTraverser
-from src.webgoose import version as webgoose_version
+
+
 
 DATE_FORMAT = '%b %d %Y, %I:%M%p'
 
 
-def last_modified(file_path, content, args):
 
-    # TODO : MAKE WORK WITH BUILD PATH, CHECK IF MARKDOWN EXISTS IF BUILD DOESN'T (build may be new and later in to-build list)
+def last_modified(page_dict, content, args):
 
-    path = args['path'] if "path" in args else file_path
+    path = page_dict['build_path']
 
     date_format = args['format'] if "format" in args else DATE_FORMAT
 
+    # Return Last Build Time If Available
+    # Otherwise, return current time (assume page source exists but later in build queue)
     if os.path.exists(path):
 
         last_mod = datetime.fromtimestamp(os.path.getmtime(path))
 
         return last_mod.strftime(DATE_FORMAT)
 
-    return ""
+    
+    return time.time()
 
 
 
 
 
-def get_version(filePath, content, args):
+
+def get_version(page_dict, content, args):
     return webgoose_version
 
 
 
 
-def table_of_contents(filePath, content, args):
+def table_of_contents(page_dict, content, args):
     return ""
 
 
 
 
-def get_time(filePath, content, args):
+def get_time(page_dict, content, args):
     
     format = args['format'] if "format" in args else DATE_FORMAT
 
@@ -51,25 +56,22 @@ def get_time(filePath, content, args):
 
 
 
-def index(file_path, content, args):
+def index(page_dict, content, args):
 
-    print(os.path.dirname(file_path))
-    
-    traverser = FileTraverser(os.path.dirname(file_path))
+    traverser = FileTraverser(os.path.dirname(page_dict['source_path']))
 
     _ , pages = traverser.find(".md")
 
-    print(pages)
+    if not "include_current" in args:
+
+        pages.remove(page_dict['filename'])
+
 
     index_list = list(map(lambda x: f"<li>{x}</li>", pages))
 
-    if not "include_current" in args:
-
-        index_list.remove(file_path)
-
-    return f"<ul> {' '.join(index_list)} </ul>"
+    return f"<ul> {''.join(index_list)} </ul>"
 
 
 
-def docroot(filePath, content, args):
-    return config['site']['doc-root']
+def docroot(page_dict, content, args):
+    return os.path.join(config['site']['doc-root'], config['build']['build-dir'])
--- src/webgoose/page_builder.py
+++ src/webgoose/page_builder.py
@@ -4,16 +4,13 @@ import re
 from typing import Dict, List, Tuple
 
 import cmarkgfm
-import frontmatter
 from bs4 import BeautifulSoup
 from cmarkgfm.cmark import Options as cmarkgfmOptions
 from jinja2 import Environment, FileSystemLoader
 
 from src.webgoose.config import config
+from src.webgoose.page import Page
 from src.webgoose.macro_processor import MacroProcessor
-
-
-
 
 
 class PageBuilder():
@@ -33,14 +30,13 @@ class PageBuilder():
         In The List Passed To The Page Builder Object
         """
 
-        for file_path in self.build_list:
-            
-            page_build = self.__build_page(file_path)
+        # Build All Pages Provided In List
+        built_pages = [self.__build_page(file_path) for file_path in self.build_list]
 
-            build_path = self.get_build_path(file_path)
 
-            self.write_build_to_file(build_path, page_build)
-
+        # Check If Anything Failed To Build
+        if False in built_pages:
+            print("Some Pages Failed To Build")
 
 
 
@@ -50,85 +46,28 @@ class PageBuilder():
 
         """Main Function To Build An Individual Page"""
 
-        metadata, content = self.get_page_content(file_path)
-        
-        metadata = self.add_missing_metadata(file_path, metadata)
+        # [Terrible Naming]
+        # Construct Page Object Using Source Path, Get All Page Content Using It (dict)
+        page_parser = Page(file_path)
+        page_dict = page_parser.get_page()
 
-        template = self.get_template(metadata['template'])
+        page_dict = self.process_macros(page_dict)
 
-        template, content = self.process_macros(file_path, template, content)
+        page_dict['content'] = self.conv_markdown_html(page_dict['content'])
 
-        content = self.conv_markdown_html(content)
+        build = self.render_page(page_dict)
 
-        return self.render_template(template, metadata, content)
-
-
-
-
-
-
-    def get_build_path(self, file_path: str) -> str:
-
-        """Work Out Build Path From Any Given Markdown Source Path"""
-
-        EXTENSION = ".html"
-
-        # Replace Source Directory with Build Directory
-        match_string = f"^{config['build']['source-dir']}"
-
-        repl_string = config["build"]["build-dir"]
-
-        build_path = re.sub(match_string, repl_string, file_path)
-
-        # Replace From Last Occurence of '.' In String Till End With Build Extension
-        last_period_index = build_path.rindex(".")
-
-        # rindex() Returns -1 If No Occurence Found
-        if last_period_index > 0:
-
-            return build_path[:last_period_index] + EXTENSION
-
-        return build_path + EXTENSION
+        self.write_build_to_file(page_dict['build_path'], build)
 
 
 
 
 
 
-    def get_filename_from_path(self, file_path: str) -> str:
-
-        """Gets Filename From Any Given Path"""
-
-        last_slash_index = file_path.rindex("/")
-
-        # Return Substring After Last Slash If Present, Else Returns Path
-        return file_path[:last_slash_index] if last_slash_index > 0 else file_path
-
-
-
-
-
-
-    def get_page_content(self, file_path: str) -> Tuple[dict[str, str], str]:
-
-        """
-        Retrieves Markdown Content as 'str' and YAML Frontmatter as 'dict'
-        From file_path Specified
-        """
-
-        with open(file_path, "r", encoding="utf-8") as file:
-            data = frontmatter.load(file)
-
-        return data.metadata, data.content
-
-
-
-
-
-
-    def write_build_to_file(self, build_path: str, page_build: str):
+    def write_build_to_file(self, build_path: str, build: str):
 
         """Write Final HTML Output For A Page To File"""
+
 
         if not os.path.exists(os.path.dirname(build_path)):
 
@@ -137,7 +76,7 @@ class PageBuilder():
 
         with open(build_path, "w", encoding="utf-8") as file:
 
-            soup = BeautifulSoup(page_build, "html.parser")
+            soup = BeautifulSoup(build, "html.parser")
 
             file.write(soup.prettify(formatter="html"))
 
@@ -146,21 +85,19 @@ class PageBuilder():
 
 
 
-    def process_macros(self, file_path, template, content):
+    def process_macros(self, page_dict: dict) -> dict:
 
         # Process Macros On Page Content
-        pre_processor = MacroProcessor(file_path, content)
-        
-        content = pre_processor.process()
+        pre_processor = MacroProcessor(page_dict, page_dict['content'])
+        page_dict['content'] = pre_processor.process()
 
         
         # Process Macros On Template, Using Content As Reference
-        pre_processor = MacroProcessor(file_path, template, content)
+        pre_processor = MacroProcessor(page_dict, page_dict['template'], page_dict['content'])
+        page_dict['template'] = pre_processor.process()
 
-        template = pre_processor.process()
 
-
-        return template, content 
+        return page_dict
 
 
 
@@ -172,53 +109,16 @@ class PageBuilder():
         Converts Markdown Documents (GitHub Flavoured) To HTML Markup
         """
 
-        # Convert Markdown to HTML, Use Safe Formatting To Avoid Injection/XSS Issues
+        # Convert Markdown to HTML, Use Unsafe Formatting 
+        # (files are generated once locally, so no real issues with XSS, etc)
         cmark_options = cmarkgfmOptions.CMARK_OPT_UNSAFE
-
         return cmarkgfm.github_flavored_markdown_to_html(content, cmark_options)
 
 
 
 
 
-    def add_missing_metadata(self, build_path: str, metadata: Dict[str, str]) -> Dict[str, str]:
-
-        """Uses Content & Filename Of Page To Add Any Missing Metadata To Page"""
-
-        # Set Title As Filename If Not Present In Metadata
-        if not "title" in metadata:
-
-            metadata["title"] = self.get_filename_from_path(build_path)
-
-
-        # Set Template To Use To The Default Set In Config If Not Set
-        if not "template" in metadata:
-
-            metadata["template"] = config["build"]["default-template"]
-
-        return metadata
-
-
-
-
-
-    def get_template(self, template_name):
-
-        if os.path.exists(f"template/{template_name}"):
-
-            with open(f"template/{template_name}", 'r', encoding='utf-8') as file:
-                
-                return file.read()
-
-        else:
-
-            return False
-
-
-
-
-
-    def render_template(self, template, metadata: Dict[str, str], content: str) -> str:
+    def render_page(self, page_dict: dict) -> str:
 
         """Insert Page Content Onto HTML Template And Renders Full HTML Document"""
 
@@ -228,11 +128,10 @@ class PageBuilder():
         )
 
         # Replace All Modulo Symbols With HTML Entity To Prevent Conflicts With Jinja2 Template Tags
-        content = content.replace("%", "&#37;")
+        page_dict['content'] = page_dict['content'].replace("%", "&#37;")
 
         # Create Template Object From Template String
-        template = jinja_env.from_string(template)
+        template = jinja_env.from_string(page_dict['template'])
 
         # Render Page, Pass Metadata Dict To Jinja2, Process Macros On Template
-        full_page = template.render({"meta": metadata, "content": content})
-        return full_page
+        return template.render({"meta": page_dict['metadata'], "content": page_dict['content']})
