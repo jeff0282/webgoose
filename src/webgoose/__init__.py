f17d45ef815973b44b9eb1dd1d66ed64c75cbb08
Travis Campbell <traviscampbell@hotmail.co.uk>
2022-11-25 21:31:42 +0000

    WILL NOT WORKgit add . Folder Restructure

--- src/webgoose/__init__.py
+++ src/webgoose/__init__.py
@@ -1,2 +1,10 @@
+
+# Initialise Version Information
 version_info = (0, 0, 2)
 version = '.'.join(str(c) for c in version_info)
+
+
+# Get Config Stuffs
+import configparser
+config = configparser.ConfigParser()
+config.read(".config/config.ini")
