f91a441a74e9ad1703b1bc9e3e29fd6986dc1652
Travis F. Campbell <tfc@webgoose.net>
2023-06-29 01:20:53 +0100

    Establishes basic schema for site.yaml

---
+++ webgoose/site.py
@@ -0,0 +1,24 @@
+
+from        pathlib     import      Path
+from        typing      import      Any
+from        typing      import      Dict
+
+
+
+class Site:
+
+
+    def __init__(self, config_file_loc: Path):
+        pass
+
+
+    def __get_config(self, config_file_loc: Path) -> Dict[str, Any]:
+        pass
+
+
+    def __build_tree(self) -> Tree:
+        pass
+
+
+    def build_site(self):
+        pass
