abee2e4d587ccb802404fe8675ff0ad36b409110
Travis F. Campbell <tfc@webgoose.net>
2023-07-12 16:32:19 +0100

    Reimplemented glob, still awaiting recursive support and testing

--- webgoose/tree/structs/dir_node.py
+++ webgoose/tree/structs/dir_node.py
@@ -3,13 +3,12 @@ import  fnmatch
 
 from    typing      import      Any
 from    typing      import      Dict
-from    typing      import      Callable
+from    typing      import      Iterable
 from    typing      import      Iterator
 from    typing      import      List
 from    typing      import      Optional
 from    typing      import      Tuple
 from    typing      import      Type
-from    typing      import      Union
 
 from    webgoose.tree.structs   import  DuplicateChildNameError
 from    webgoose.tree.structs   import  NodeNotFoundError
@@ -28,22 +27,21 @@ class Directory(TreeNode):
                  name:      str,
                  parent:    Optional[Type[TreeNode]]    = None,
                  metadata:  Dict[str, Any]              = dict(),
-                 children:  Tuple[Type[TreeNode]]       = tuple()) -> None:
+                 children:  Iterable[Type[TreeNode]]    = tuple()) -> None:
         """
         Create a Directory Node
         """
 
         # Initialise private vars with default values
          # (second item in name stack left blank as placeholder for actual name)
-        self._name_stack = [self.ROOT_DIR_NAME, ""]
+        self._name_stack = [self.ROOT_DIR_NAME, None]
+        self._children: List[TreeNode] = []
 
         # Create The Base TreeNode
         super().__init__(name, parent, metadata)
 
         # Add Children
-        self._children: List[TreeNode] = []
-        for child in children:
-            self.add(child)
+        self.add(*children)
 
 
     
@@ -130,22 +128,22 @@ class Directory(TreeNode):
 
     
 
-    def add(self, *nodes_to_add: Tuple[Type[TreeNode]]) -> None:
+    def add(self, *nodes: Tuple[Type[TreeNode]]) -> None:
         """ 
-        Add N number of nodes as children of this node 
+        Add N number of nodes as children of this node
         
         Throws DuplicateChildNameError in the event of a name conflict
         betwewwn a current and new child
         """
 
-        for node in nodes_to_add:
+        for node in nodes:
             self._add_node(node)
 
 
 
     def get(self, path: str) -> Type[TreeNode]:
         """ 
-        Get child node by path 
+        Get node by path 
         
         Throws NodeNotFoundError if path doesn't exist
         """
@@ -180,7 +178,71 @@ class Directory(TreeNode):
         Implements glob for site tree
         """
 
-        # TODO: _traverse_to_path(parts, func) no longer in private API
+
+        def helper_func(starting_node:      Type['TreeNode'],
+                        path_parts:         Tuple[str, ...]) -> Tuple[Type[TreeNode], ...]:
+            
+            """
+            Helper function to allow for recursive calls (**) part-way through
+            the globbing process :)
+            """
+
+            # start globing :3
+            partial_matches: List[Type[TreeNode]] = [starting_node]
+
+            # try to match each path part going down the tree
+            for name in path_parts:
+
+                # Before looping, filter partial_matches of non-directories
+                # non-directories should only be allowed on last iter
+                partial_matches = filter(lambda node: isinstance(node, Directory), partial_matches)
+
+                # Recursive glob case
+                if name == "**":
+                    # TODO: awaiting .map_down() support
+                    pass
+
+                # Non-recursive case
+                # Check if already matched nodes continue to match the
+                # path by checking their children's names, filter out if not
+                old_partial_matches = partial_matches
+                partial_matches = []
+                for node in old_partial_matches:
+                    partial_matches.extend(child for child in node.children 
+                                           if fnmatch.fnmatch(child.name, name))   
+                    
+                # if all partial matches have been filtered out, end loop
+                if not partial_matches:
+                    break;
+            
+            # Return final partial_matches, convert to tuple    
+            return tuple(partial_matches)
+
+
+        # split path pattern into it's parts
+        is_absolute, path_parts = self._split_path(pattern)
+
+        # set starting node
+        starting_node = self.root if is_absolute else self
+
+        # Use helper function to glob using above
+        return helper_func(starting_node, path_parts)
+        
+
+
+                
+
+
+
+
+                    
+
+
+
+
+
+
+
 
     
 
